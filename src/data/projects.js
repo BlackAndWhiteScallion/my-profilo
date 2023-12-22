1a5bd191976380e4a2b2920376addc16d4e08a59
BlackAndWhiteScallion <tanguo@hotmail.com>
2023-12-21 23:48:46 -0500

    update

---
+++ src/data/projects.js
@@ -0,0 +1,25 @@
+// Projects data.
+// Note: Here we are getting data from a js file, but in a different project I'll be fetching these projects from some srt of APIs.
+
+const projects = [
+	{
+		id: 1,
+		title: 'Google Health Platform',
+		category: 'Web Application',
+		img: require('@/assets/images/web-project-2.jpg'),
+	},
+	{
+		id: 2,
+		title: 'Phoenix Digital Agency',
+		category: 'Mobile Application',
+		img: require('@/assets/images/mobile-project-2.jpg'),
+	},
+	{
+		id: 3,
+		title: 'Project Management UI',
+		category: 'UI/UX Design',
+		img: require('@/assets/images/ui-project-1.jpg'),
+	},
+];
+
+export default projects;
---
+++ src/data/socialLinks.js
@@ -0,0 +1,20 @@
+export const socialLinks = [
+	{
+		id: 1,
+		name: 'Website',
+		icon: 'globe',
+		url: 'https://www.stoman.me/',
+	},
+	{
+		id: 2,
+		name: 'GitHub',
+		icon: 'github',
+		url: 'https://github.com/BlackAndWhiteScallion',
+	},
+	{
+		id: 3,
+		name: 'LinkedIn',
+		icon: 'linkedin',
+		url: 'https://www.linkedin.com/in/tan-guo/',
+	},
+];
